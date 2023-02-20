8e3f7248fcefd0f5fd1623dcbe34b265ccdbd1ff
reihaneh <reihaneh763@gmail.com>
2023-02-20 22:24:45 +0330

    add ExpertSubServiceException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ExpertSubServiceException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ExpertSubServiceException extends RuntimeException {
+    public ExpertSubServiceException(String message) {
+        super(message);
+    }
+}
