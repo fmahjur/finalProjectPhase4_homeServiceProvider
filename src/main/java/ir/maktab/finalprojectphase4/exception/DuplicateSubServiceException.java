a6fe2066132f4c8e24ac424dfcf59a6ee0c90606
reihaneh <reihaneh763@gmail.com>
2023-02-20 20:33:46 +0330

    add DuplicateSubServiceException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/DuplicateSubServiceException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class DuplicateSubServiceException extends RuntimeException {
+    public DuplicateSubServiceException(String message) {
+        super(message);
+    }
+}
