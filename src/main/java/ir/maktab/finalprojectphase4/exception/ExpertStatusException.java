920639da8058f80d72e346f702aeddbfa5095543
reihaneh <reihaneh763@gmail.com>
2023-02-20 22:09:39 +0330

    add ExpertStatusException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ExpertStatusException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ExpertStatusException extends RuntimeException {
+    public ExpertStatusException(String message) {
+        super(message);
+    }
+}
