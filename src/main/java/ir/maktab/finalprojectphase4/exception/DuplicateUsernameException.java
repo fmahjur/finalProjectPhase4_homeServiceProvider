eeb355070355887ced3b6810ead6afd17c63265c
reihaneh <reihaneh763@gmail.com>
2023-02-20 21:05:55 +0330

    add DuplicateUsernameException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/DuplicateUsernameException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class DuplicateUsernameException extends RuntimeException {
+    public DuplicateUsernameException(String message) {
+        super(message);
+    }
+}
