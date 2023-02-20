7c8096810e55f966392d1bcb8830b3ad0bc92718
reihaneh <reihaneh763@gmail.com>
2023-02-20 21:56:34 +0330

    add RegistrationException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
