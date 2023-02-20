665b045155a4cab9a9543a93d75f7976439118df
reihaneh <reihaneh763@gmail.com>
2023-02-20 20:07:56 +0330

    add DuplicateBaseServiceException class

--- src/main/java/ir/maktab/finalprojectphase4/data/model/Service.java
+++
@@ -1,5 +0,0 @@
-package ir.maktab.finalprojectphase4.data.model;
-
-public interface Service {
-    public void showServiceDetails();
-}
---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/DuplicateBaseServiceException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class DuplicateBaseServiceException extends RuntimeException {
+    public DuplicateBaseServiceException(String message) {
+        super(message);
+    }
+}
