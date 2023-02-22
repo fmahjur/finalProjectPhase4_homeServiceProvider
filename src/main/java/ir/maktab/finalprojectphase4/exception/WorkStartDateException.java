95a726e2a8e6706fd2bcf7d97b19f71f1e88c836
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:48:23 +0330

    add WorkStartDateException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/WorkStartDateException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class WorkStartDateException extends RuntimeException {
+    public WorkStartDateException(String message) {
+        super(message);
+    }
+}
