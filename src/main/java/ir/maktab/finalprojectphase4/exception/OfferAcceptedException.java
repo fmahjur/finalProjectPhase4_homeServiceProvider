58e02deedc132bf2053971b2e3e7d849d5d2fcf8
reihaneh <reihaneh763@gmail.com>
2023-02-22 20:01:59 +0330

    add OfferAcceptedException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/OfferAcceptedException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class OfferAcceptedException extends RuntimeException {
+    public OfferAcceptedException(String message) {
+        super(message);
+    }
+}
