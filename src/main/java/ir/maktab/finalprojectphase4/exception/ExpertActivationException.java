0b3414e269df9ea3dc5bb637a256e1a60aa3b1e8
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:44:23 +0330

    add ExpertActivationException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ExpertActivationException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ExpertActivationException extends RuntimeException {
+    public ExpertActivationException(String message) {
+        super(message);
+    }
+}
