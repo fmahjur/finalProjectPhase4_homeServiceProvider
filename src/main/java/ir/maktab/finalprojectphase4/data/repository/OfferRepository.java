8eb63283b3e2dec22bfe29698803b7d91c3e3d3b
reihaneh <reihaneh763@gmail.com>
2023-02-22 21:32:02 +0330

    add OfferRepository class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/repository/OfferRepository.java
@@ -0,0 +1,32 @@
+package ir.maktab.finalprojectphase4.data.repository;
+
+import ir.maktab.finalprojectphase4.data.enums.OfferStatus;
+import ir.maktab.finalprojectphase4.data.model.Expert;
+import ir.maktab.finalprojectphase4.data.model.Offer;
+import ir.maktab.finalprojectphase4.data.model.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+    List<Offer> findAllByOrderIs(Orders order);
+
+    List<Offer> findAllByExpert(Expert expert);
+
+    @Query("select o from Offer o where o.isDeleted= :isDeleted")
+    List<Offer> findAllByDeletedIs(boolean isDeleted);
+
+    @Query("select o from Offer o where o.order= :order order by o.offerPrice")
+    List<Offer> findByOrdersSortByOfferPrice(Orders order);
+
+    @Query("select o from Offer o where o.order= :order order by o.expert.rate")
+    List<Offer> findByOrdersSortByExOrderByExpertRate(Orders order);
+
+    @Query("select o from Offer o where o.order= :order order by o.offerPrice, o.expert.rate")
+    List<Offer> findByOrdersSortByOfferPriceAndExpertRate(Orders order);
+
+    List<Offer> findOffersByExpertAndOfferStatus(Expert expert, OfferStatus offerStatus);
+}
