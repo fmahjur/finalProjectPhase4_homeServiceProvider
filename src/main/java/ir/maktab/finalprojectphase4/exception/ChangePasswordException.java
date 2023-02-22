6749cee1c12a3753cb381cb7ef126ff50fd37f82
reihaneh <reihaneh763@gmail.com>
2023-02-22 13:52:01 +0330

    add ChangePasswordException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ChangePasswordException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ChangePasswordException extends RuntimeException {
+    public ChangePasswordException(String message) {
+        super(message);
+    }
+}
