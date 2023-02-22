36b84f4abb32a034d15b0a5990fd95962c767fe0
reihaneh <reihaneh763@gmail.com>
2023-02-23 00:37:52 +0330

    add AdminController class

---
+++ src/main/java/ir/maktab/finalprojectphase4/controller/AdminController.java
@@ -0,0 +1,121 @@
+package ir.maktab.finalprojectphase4.controller;
+
+import ir.maktab.finalprojectphase4.data.dto.request.*;
+import ir.maktab.finalprojectphase4.data.dto.response.*;
+import ir.maktab.finalprojectphase4.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final AdminService adminService;
+
+    @PostMapping("/add-base-service")
+    public void addMainService(@RequestBody BaseServiceRequestDTO baseServiceRequestDTO) {
+        adminService.addNewService(baseServiceRequestDTO);
+    }
+
+    @DeleteMapping("/delete-base-service/{id}")
+    public void deleteMainService(@PathVariable Long id) {
+        adminService.removeService(id);
+    }
+
+    @PostMapping("/add-sub-service")
+    public void addSubService(@RequestBody SubServiceRequestDTO subServiceRequestDTO) {
+        adminService.addNewSubService(subServiceRequestDTO);
+    }
+
+    @PostMapping("/allocation-service-to-expert")
+    public void allocationServiceToExpert(@RequestBody ExpertSubServiceDTO expertSubServiceDTO) {
+        adminService.allocationServiceToExpert(expertSubServiceDTO);
+    }
+
+    @DeleteMapping("/remove-service-from-expert")
+    public void deleteServiceFromExpert(@RequestBody ExpertSubServiceDTO expertSubServiceDTO) {
+        adminService.removeExpertFromService(expertSubServiceDTO);
+    }
+
+    @GetMapping("/show-all-services")
+    public List<BaseServiceResponseDTO> findAllBaseService() {
+        return adminService.showAllService();
+    }
+
+    @GetMapping("/show-all-sub-services")
+    public List<SubServiceResponseDTO> findAllSubService() {
+        return adminService.showSubServices();
+    }
+
+    @GetMapping("/show-all-sub-services-by-service/{baseServiceId}")
+    public List<SubServiceResponseDTO> findAllSubServiceByService(@PathVariable Long baseServiceId) {
+        return adminService.showSubServicesByService(baseServiceId);
+    }
+
+    @PutMapping("/edit-sub-service")
+    public void editSubServiceBasePriceAndDescription(@RequestBody UpdateSubServiceDTO updateSubServiceDTO) {
+        adminService.editSubService(updateSubServiceDTO);
+    }
+
+    @GetMapping("/show-all-expert")
+    public List<ExpertResponseDTO> findAllExpert() {
+        return adminService.showAllExpert();
+    }
+
+    @PutMapping("/confirm-expert/{expertId}")
+    public void confirmExpert(@PathVariable Long expertId) {
+        adminService.confirmExpert(expertId);
+    }
+
+    @PutMapping("/change-expert-status/{expertId}")
+    public void changeExpertStatus(@RequestBody ChangeExpertStatusDTO changeExpertStatusDTO) {
+        adminService.changeExpertStatus(changeExpertStatusDTO);
+    }
+
+    @PutMapping("/check-expert-delay/{offerId}")
+    public void checkExpertDelayForDoingWork(@PathVariable Long offerId) {
+        adminService.checkExpertDelayForWork(offerId);
+    }
+
+    @PutMapping("/change-expert-activation/{expertId}")
+    public void inactiveExpertAccount(@PathVariable Long expertId) {
+        adminService.deActiveExpert(expertId);
+    }
+
+    @PostMapping("/filter-experts")
+    public List<FilterExpertResponseDTO> expertsFilter(@RequestBody FilterExpertDTO expertDTO) {
+        return adminService.expertFilter(expertDTO);
+    }
+
+    @PostMapping("/filter-customers")
+    public List<FilterCustomerResponseDTO> customersFilter(@RequestBody FilterCustomerDTO customerDTO) {
+        return adminService.customerFilter(customerDTO);
+    }
+
+    @GetMapping("/show-sub-services-expert/{subServiceId}")
+    public List<ExpertResponseDTO> viewSubServiceExperts(@PathVariable Long subServiceId) {
+        return adminService.showSubServicesExpert(subServiceId);
+    }
+
+    @GetMapping("/show-number-of-customers-registered-orders/{customerId}")
+    public int viewNumberOfCustomersRegisteredOrders(@PathVariable Long customerId) {
+        return adminService.showNumberOfCustomersRegisteredOrders(customerId);
+    }
+
+    @GetMapping("/show-number-of-customers-registered-orders-by-order-status")
+    public int viewNumberOfCustomersRegisteredOrdersByOrderStatus(@RequestBody CustomerOrdersStatusDTO customerOrdersStatusDTO) {
+        return adminService.showNumberOfCustomersRegisteredOrdersByOrderStatus(customerOrdersStatusDTO);
+    }
+
+    @GetMapping("/show-number-of-experts-placed-offers/{expertId}")
+    public int viewNumberOfExpertsPlacedOffers(@PathVariable Long expertId) {
+        return adminService.showNumberOfExpertsPlacedOffers(expertId);
+    }
+
+    @GetMapping("/show-number-of-experts-placed-offers-by-offer-status")
+    public int viewNumberOfExpertsPlacedOffersByOfferStatus(@RequestBody ExpertOffersStatusDTO expertOffersStatusDTO) {
+        return adminService.showNumberOfExpertsPlacedOffersByOfferStatus(expertOffersStatusDTO);
+    }
+}
