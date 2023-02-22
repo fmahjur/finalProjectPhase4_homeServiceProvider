ec45e59b326f6363a563dcc6c8c19fef1a908c38
reihaneh <reihaneh763@gmail.com>
2023-02-23 00:51:20 +0330

    add PayByCreditDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/PayByCreditDTO.java
@@ -0,0 +1,15 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PayByCreditDTO {
+    Long orderId;
+    Long expertId;
+    Long amount;
+}
