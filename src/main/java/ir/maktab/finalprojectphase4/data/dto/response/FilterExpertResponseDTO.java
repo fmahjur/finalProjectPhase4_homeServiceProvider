9ab118225ac59e017526122af5a999b52048cd05
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:43:21 +0330

    add FilterExpertResponseDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/response/FilterExpertResponseDTO.java
@@ -0,0 +1,21 @@
+package ir.maktab.finalprojectphase4.data.dto.response;
+
+import ir.maktab.finalprojectphase4.data.enums.ExpertStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FilterExpertResponseDTO {
+    String firstname;
+    String lastname;
+    String email;
+    String username;
+    Boolean isActive;
+    ExpertStatus expertStatus;
+    Double rate;
+    Long credit;
+}
