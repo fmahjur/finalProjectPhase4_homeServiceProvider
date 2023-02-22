cc200e6e761ed7a2190eb6a6aa2378c98da049bc
reihaneh <reihaneh763@gmail.com>
2023-02-22 14:00:59 +0330

    add ExpertHistoryDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/ExpertHistoryDTO.java
@@ -0,0 +1,14 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExpertHistoryDTO {
+    Long expertId;
+    boolean isAccept;
+}
