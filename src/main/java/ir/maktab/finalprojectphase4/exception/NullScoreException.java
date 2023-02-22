69b4bf3a95bcc5cb684d74309891de501ceecd3a
reihaneh <reihaneh763@gmail.com>
2023-02-22 19:42:33 +0330

    add NullScoreException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/NullScoreException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class NullScoreException extends RuntimeException {
+    public NullScoreException(String message) {
+        super(message);
+    }
+}
