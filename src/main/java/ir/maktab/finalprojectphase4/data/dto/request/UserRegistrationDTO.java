b9ea627b7975255081c96f91f19afb20cccbe1f7
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:37:22 +0330

    add UserRegistrationDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/UserRegistrationDTO.java
@@ -0,0 +1,23 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserRegistrationDTO {
+    String firstname;
+    String lastname;
+    @Email(message = "invalid Email!")
+    String email;
+    String username;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{8,20}$",
+            message = "invalid password!")
+    String password;
+    Long credit;
+}
