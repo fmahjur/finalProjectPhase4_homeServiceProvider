c4226b08eacce471317c30345d81916845247fce
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:42:17 +0330

    add CustomerResponseDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/response/CustomerResponseDTO.java
@@ -0,0 +1,15 @@
+package ir.maktab.finalprojectphase4.data.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CustomerResponseDTO {
+    String firstname;
+    String lastname;
+    String emailAddress;
+}
