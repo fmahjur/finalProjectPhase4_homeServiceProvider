937e7e3dcaeb7b58c08600b0aea6cff960328ccf
reihaneh <reihaneh763@gmail.com>
2023-02-22 19:45:09 +0330

    add ScoreOutsideDefinedRangeException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ScoreOutsideDefinedRangeException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ScoreOutsideDefinedRangeException extends RuntimeException {
+    public ScoreOutsideDefinedRangeException(String message) {
+        super(message);
+    }
+}
