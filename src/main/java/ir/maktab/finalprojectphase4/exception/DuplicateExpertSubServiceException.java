1ca6d958c39014a110cc8dd3d25bb0e2daa00d5e
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:41:56 +0330

    add DuplicateExpertSubServiceException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/DuplicateExpertSubServiceException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class DuplicateExpertSubServiceException extends RuntimeException {
+    public DuplicateExpertSubServiceException(String message) {
+        super(message);
+    }
+}
