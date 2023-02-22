e70c44003783f2fe6151a864bf5ef301dce9f586
reihaneh <reihaneh763@gmail.com>
2023-02-22 13:54:52 +0330

    add ChangePasswordDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/ChangePasswordDTO.java
@@ -0,0 +1,19 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChangePasswordDTO {
+    String username;
+    String password;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{8,20}$",
+            message = "invalid password!")
+    String newPassword;
+    String confirmNewPassword;
+}
