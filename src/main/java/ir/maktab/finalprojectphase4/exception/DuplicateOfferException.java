973b72c3e6dac91a7ea879994442804430a8a51c
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:36:16 +0330

    add DuplicateOfferException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/DuplicateOfferException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class DuplicateOfferException extends RuntimeException {
+    public DuplicateOfferException(String message) {
+        super(message);
+    }
+}
