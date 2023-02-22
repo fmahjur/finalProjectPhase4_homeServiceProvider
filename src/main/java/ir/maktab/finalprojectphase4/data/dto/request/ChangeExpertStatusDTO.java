4196226ad7980de9be4bcc7a16d474e54511c290
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:05:01 +0330

    add ChangeExpertStatusDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/ChangeExpertStatusDTO.java
@@ -0,0 +1,15 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import ir.maktab.finalprojectphase4.data.enums.ExpertStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChangeExpertStatusDTO {
+    Long expertId;
+    ExpertStatus expertStatus;
+}
