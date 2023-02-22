5412484e158aaf2befa26a52e115ddd5659ac901
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:40:22 +0330

    add OfferRequestDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/OfferRequestDTO.java
@@ -0,0 +1,26 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OfferRequestDTO {
+    Long orderId;
+    Long offerId;
+    Long expertID;
+    Long offerPrice;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][.SS][.S]")
+    LocalDateTime proposedStartDate;
+    int durationOfWork;
+}
