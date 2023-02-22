828b737466600ebd79f62bcac3c00d06722a9a78
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:50:40 +0330

    add ImageFileException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ImageFileException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ImageFileException extends RuntimeException {
+    public ImageFileException(String message) {
+        super(message);
+    }
+}
