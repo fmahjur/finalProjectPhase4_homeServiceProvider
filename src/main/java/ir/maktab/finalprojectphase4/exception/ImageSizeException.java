4fb41e90a8fd0e7e6a83948f6dbacc079fec6e23
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:51:39 +0330

    add ImageSizeException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ImageSizeException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ImageSizeException extends RuntimeException {
+    public ImageSizeException(String message) {
+        super(message);
+    }
+}
