86fd9c7c82fa5b6289f6105dd9d6e859f24c40bf
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:27:16 +0330

    add InvalidProposedPriceException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/InvalidProposedPriceException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class InvalidProposedPriceException extends RuntimeException {
+    public InvalidProposedPriceException(String message) {
+        super(message);
+    }
+}
