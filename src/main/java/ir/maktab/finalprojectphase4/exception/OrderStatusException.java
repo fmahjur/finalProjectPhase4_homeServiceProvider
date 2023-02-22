04d23a1c79a22032ed6a4b67b611006a6a35a001
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:19:50 +0330

    add OrderStatusException class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/OrderStatusException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class OrderStatusException extends RuntimeException{
+    public OrderStatusException(String message) {
+        super(message);
+    }
+}
