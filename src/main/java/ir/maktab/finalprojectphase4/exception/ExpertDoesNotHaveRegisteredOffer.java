8069a51ee9837d4fb30338d1c9d6796bda91d067
reihaneh <reihaneh763@gmail.com>
2023-02-23 00:10:42 +0330

    add ExpertDoesNotHaveRegisteredOffer class

---
+++ src/main/java/ir/maktab/finalprojectphase4/exception/ExpertDoesNotHaveRegisteredOffer.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase4.exception;
+
+public class ExpertDoesNotHaveRegisteredOffer extends RuntimeException {
+    public ExpertDoesNotHaveRegisteredOffer(String message) {
+        super(message);
+    }
+}
