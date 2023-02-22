35da061b0b7bf8765b3ce3e29d8823765eff45ee
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:56:52 +0330

    add Config class

---
+++ src/main/java/ir/maktab/finalprojectphase4/config/Config.java
@@ -0,0 +1,31 @@
+package ir.maktab.finalprojectphase4.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public JavaMailSenderImpl javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
