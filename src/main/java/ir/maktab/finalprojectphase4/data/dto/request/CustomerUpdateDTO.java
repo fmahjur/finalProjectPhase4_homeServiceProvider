fa4f3a8beb2e9c21f22486809862514f78fa7623
reihaneh <reihaneh763@gmail.com>
2023-02-23 01:56:12 +0330

    add CustomerUpdateDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/CustomerUpdateDTO.java
@@ -0,0 +1,19 @@
+package ir.maktab.finalprojectphase4.data.dto.request;
+
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CustomerUpdateDTO {
+    String firstname;
+    String lastname;
+    @Email
+    String emailAddress;
+    String username;
+    Long credit;
+}
