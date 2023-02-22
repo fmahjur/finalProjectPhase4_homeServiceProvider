66e3009a8ab3bf4c5b959445716f0e6c9fac0774
reihaneh <reihaneh763@gmail.com>
2023-02-22 22:45:24 +0330

    add OrderHistoryDTO class

--- src/main/java/ir/maktab/finalprojectphase4/data/dto/request/OrderHistoryDTO.java
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/request/OrderHistoryDTO.java
@@ -9,6 +9,6 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ExpertHistoryDTO {
+public class OrderHistoryDTO {
     OrderStatus orderStatus;
 }
