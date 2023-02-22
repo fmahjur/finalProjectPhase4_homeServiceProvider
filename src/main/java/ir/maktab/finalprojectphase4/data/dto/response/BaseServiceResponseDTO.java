c4289cdfe201f5d6bdc61b72d7c21f3819268841
reihaneh <reihaneh763@gmail.com>
2023-02-23 00:55:16 +0330

    add BaseServiceResponseDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase4/data/dto/response/BaseServiceResponseDTO.java
@@ -0,0 +1,13 @@
+package ir.maktab.finalprojectphase4.data.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BaseServiceResponseDTO {
+    String name;
+}
